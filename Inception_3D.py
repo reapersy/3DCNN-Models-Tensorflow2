eff8baa7b033495cf7e0d073eeac87e0b14b68a9
reapersy <reapersoy@proton.me>
2022-01-02 15:20:16 +0000

    Added Screenshots

--- Inception_3D.py
+++ Inception_3D.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Conv3D, MaxPooling3D, Dense, GlobalMaxPooling3D, Dropout, BatchNormalization,Concatenate, AveragePooling3D, Activation
-from tensor
+from tensorflow.keras.optimizers import Adam
+from config import *
+
+def conv_bn_relu(x, filters, kernel_size=(3, 3, 3
