d92fdd73d9d182b85049be8b4eb916d70ea24f5a
reapersy <reapersoy@proton.me>
2022-03-15 06:47:37 +0000

    CIFS: Fix SMB2 readdir error handling

--- Inception_3D.py
+++ Inception_3D.py
@@ -40,4 +40,8 @@ def inception_block(x, filters=256):
     b3 = AveragePooling3D(pool_size=(3, 3, 3), strides=(1, 1, 1), padding='same')(x)
     b3 = conv_bn_relu(b3, filters=filters, kernel_size=(1, 1, 1))
 
-    bs = [b0, b1, b2, 
+    bs = [b0, b1, b2, b3]
+
+    print('inception_block')
+    print(b0.get_shape())
+    print(b1.get_shape())
