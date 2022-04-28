d6a5c33a554bde17db716518352b2a4df09d72e3
reapersy <reapersoy@proton.me>
2022-04-28 16:54:45 +0000

    Added composer file

--- Inception_3D.py
+++ Inception_3D.py
@@ -66,4 +66,6 @@ def reduction_block(x, filters=256):
 
     b1 = conv_bn_relu(x, filters=filters, kernel_size=(1, 1, 1))
     b1 = conv_bn_relu(b1, filters=filters, kernel_size=(3, 3, 3))
-    b1 = conv_bn_relu(b1, filters=
+    b1 = conv_bn_relu(b1, filters=filters, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding='same')
+
+    b2 = MaxPooling3D(po
