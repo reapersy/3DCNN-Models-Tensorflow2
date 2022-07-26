c2eb36f9bf4e79a6dd6ac1512bc3c11159840b72
reapersy <reapersoy@proton.me>
2022-07-26 08:53:49 +0000

    Prepare 3.0.2 release

--- Resnet_3D.py
+++ Resnet_3D.py
@@ -1,2 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-import t
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from config import*
+from loss_fu
