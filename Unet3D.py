9ef86c1e2f7a95a2c3750ce652720cfe78ff520c
reapersy <reapersoy@proton.me>
2023-04-18 10:33:23 +0000

    holoirc: bump version

--- Unet3D.py
+++ Unet3D.py
@@ -1,3 +1,8 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Conv3D, Input, MaxPooling3D, Dropout, concatenate, UpSampling3D
+import tensorflow as tf
+
+def Unet3D(inputs,num_classes):
+    x=inputs
+    conv1 = Conv3D(8, 3, ac
