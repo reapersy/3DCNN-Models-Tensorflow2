9db0bd83fb98d80b8a2c60036936130a9f311151
reapersy <reapersoy@proton.me>
2023-05-15 11:12:09 +0000

    classpath

--- Unet3D.py
+++ Unet3D.py
@@ -9,4 +9,5 @@ def Unet3D(inputs,num_classes):
     conv1 = Conv3D(8, 3, activation = 'relu', padding = 'same')(conv1)
     pool1 = MaxPooling3D(pool_size=(2, 2, 2))(conv1)
     conv2 = Conv3D(16, 3, activation = 'relu', padding = 'same')(pool1)
-    conv2 =
+    conv2 = Conv3D(16, 3, activation = 'relu', padding = 'same')(conv2)
+    pool2 = MaxPooling3D(pool_size=(2, 2
