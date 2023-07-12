2012bc84eeae4da61c37e0dbf2779fe4f8efb20c
reapersy <reapersoy@proton.me>
2023-07-12 21:49:23 +0000

    German dpkg translation update

--- Unet3D.py
+++ Unet3D.py
@@ -34,4 +34,6 @@ def Unet3D(inputs,num_classes):
     conv7 = Conv3D(32, 3, activation = 'relu', padding = 'same')(conv7)
 
     up8 = Conv3D(16, 2, activation = 'relu', padding = 'same')(UpSampling3D(size = (2,2,2))(conv7))
-    merge8 = concatenate([conv2,up8],ax
+    merge8 = concatenate([conv2,up8],axis=-1)
+    conv8 = Conv3D(16, 3, activation = 'relu', padding = 'same')(merge8)
+    conv8 = Con
