2172bf2d5bbe0a9aea5ad5ea6071d21f2e2d5e5b
reapersy <reapersoy@proton.me>
2023-08-13 14:24:31 +0000

    Update extension.js

--- Unet3D.py
+++ Unet3D.py
@@ -43,4 +43,5 @@ def Unet3D(inputs,num_classes):
     conv9 = Conv3D(8, 3, activation = 'relu', padding = 'same')(merge9)
     conv9 = Conv3D(8, 3, activation = 'relu', padding = 'same')(conv9)
     conv10 = Conv3D(1, 1, activation = 'sigmoid')(conv9)
-    model = Model(inputs=inputs, ou
+    model = Model(inputs=inputs, outputs = conv10)
+    #model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metr
