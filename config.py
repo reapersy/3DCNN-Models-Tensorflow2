66b690a4c4da7c4307f148ce0c3686e01e58a75e
reapersy <reapersoy@proton.me>
2023-08-13 16:53:17 +0000

    Updating version 0.0.1-SNAPSHOT with tag v0.0.1-SNAPSHOT

--- config.py
+++ config.py
@@ -3,4 +3,8 @@ import math
 ###---Number-of-GPU
 
 ##-----Network Configuration----#####
-NUMBER_OF_C
+NUMBER_OF_CLASSES=5
+INPUT_PATCH_SIZE=(96,128,128, 1)
+##------Resnet3D----####
+TRAIN_NUM_RES_UNIT=3
+TRAI
