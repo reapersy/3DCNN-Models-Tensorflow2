d299d82b08033c39a973eee37c5edb947778e52e
reapersy <reapersoy@proton.me>
2023-08-28 13:21:02 +0000

    removed some dead code

--- config.py
+++ config.py
@@ -8,4 +8,6 @@ INPUT_PATCH_SIZE=(96,128,128, 1)
 ##------Resnet3D----####
 TRAIN_NUM_RES_UNIT=3
 TRAIN_NUM_FILTERS=(16, 32, 64, 128)
-TRAIN_STRIDES=((1, 1, 1), (2, 2, 2), (2, 2, 2), (2, 2,
+TRAIN_STRIDES=((1, 1, 1), (2, 2, 2), (2, 2, 2), (2, 2, 2))
+TRAIN_CLASSIFY_ACTICATION=tf.nn.relu6
+TRAIN_KERNAL_INITIALIZER=tf.keras.initializers.Vari
